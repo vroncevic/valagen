23f04f7ba3b61b0a932300aac8c5a33585b61619
Vladimir Roncevic <elektron.ronca@gmail.com>
2018-02-27 07:17:38 +0100

    [valagen] Updated calls from sh_util, updated README.md

--- README.md
+++ README.md
@@ -1,35 +1,32 @@
-Generating Vala Project (Bash script).
-================================================================================
+# Generating Vala Project (Bash script).
 
 The README is used to introduce the tool and provide instructions on
 how to install the tool, any machine dependencies it may have and any
 other information that should be provided before the tool is installed.
 
-INSTALLATION
+### INSTALLATION
 
 To install this tool type the following:
 
-	cp -R ~/valagen/bin/   /root/scripts/valagen/ver.1.0/
+```
+cp -R ~/valagen/bin/   /root/scripts/valagen/ver.1.0/
+cp -R ~/valagen/conf/  /root/scripts/valagen/ver.1.0/
+cp -R ~/valagen/log/   /root/scripts/valagen/ver.1.0/
+```
 
-	cp -R ~/valagen/conf/  /root/scripts/valagen/ver.1.0/
-
-	cp -R ~/valagen/log/   /root/scripts/valagen/ver.1.0/
-
-
-DEPENDENCIES
+### DEPENDENCIES
 
 This tool requires these other modules and libraries:
 
-	sh_util https://github.com/vroncevic/sh_util
+	* sh_util https://github.com/vroncevic/sh_util
 
-COPYRIGHT AND LICENCE
+### COPYRIGHT AND LICENCE
 
-Copyright (C) 2017 by https://github.com/vroncevic/valagen
+Copyright (C) 2018 by https://vroncevic.github.io/valagen
 
 This tool is free software; you can redistribute it and/or modify
 it under the same terms as Bash itself, either Bash version 4.2.47 or,
 at your option, any later version of Bash 4 you may have available.
 
-![alt tag](https://raw.githubusercontent.com/vroncevic/valagen/master/bash_logo.png)
-![alt tag](https://raw.githubusercontent.com/vroncevic/valagen/master/linux_logo.jpg)
+:sparkles:
 
--- bin/valagen.sh
+++ bin/valagen.sh
@@ -11,14 +11,14 @@ UTIL_VERSION=ver.1.0
 UTIL=${UTIL_ROOT}/sh_util/${UTIL_VERSION}
 UTIL_LOG=${UTIL}/log
 
-.	${UTIL}/bin/devel.sh
-.	${UTIL}/bin/usage.sh
-.	${UTIL}/bin/check_root.sh
-.	${UTIL}/bin/check_tool.sh
-.	${UTIL}/bin/logging.sh
-.	${UTIL}/bin/load_conf.sh
-.	${UTIL}/bin/load_util_conf.sh
-.	${UTIL}/bin/progress_bar.sh
+.    ${UTIL}/bin/devel.sh
+.    ${UTIL}/bin/usage.sh
+.    ${UTIL}/bin/check_root.sh
+.    ${UTIL}/bin/check_tool.sh
+.    ${UTIL}/bin/logging.sh
+.    ${UTIL}/bin/load_conf.sh
+.    ${UTIL}/bin/load_util_conf.sh
+.    ${UTIL}/bin/progress_bar.sh
 
 VALAGEN_TOOL=valagen
 VALAGEN_VERSION=ver.1.0
@@ -28,25 +28,25 @@ VALAGEN_UTIL_CFG=${VALAGEN_HOME}/conf/${VALAGEN_TOOL}_util.cfg
 VALAGEN_LOG=${VALAGEN_HOME}/log
 
 declare -A VALAGEN_USAGE=(
-	[USAGE_TOOL]="__${VALAGEN_TOOL}"
-	[USAGE_ARG1]="[PROJECT_NAME] Name of project"
-	[USAGE_ARG2]="[PROJECT_PATH] Project root folder"
-	[USAGE_ARG3]="[COMMENT] Short description"
-	[USAGE_EX_PRE]="# Generating vala project"
-	[USAGE_EX]="__${VALAGEN_TOOL} ftool /opt/ \"Font generator\""
+    [USAGE_TOOL]="${VALAGEN_TOOL}"
+    [USAGE_ARG1]="[PROJECT_NAME] Name of project"
+    [USAGE_ARG2]="[PROJECT_PATH] Project root folder"
+    [USAGE_ARG3]="[COMMENT] Short description"
+    [USAGE_EX_PRE]="# Generating vala project"
+    [USAGE_EX]="${VALAGEN_TOOL} ftool /opt/ \"Font generator\""
 )
 
 declare -A VALAGEN_LOGGING=(
-	[LOG_TOOL]="${VALAGEN_TOOL}"
-	[LOG_FLAG]="info"
-	[LOG_PATH]="${VALAGEN_LOG}"
-	[LOG_MSGE]="None"
+    [LOG_TOOL]="${VALAGEN_TOOL}"
+    [LOG_FLAG]="info"
+    [LOG_PATH]="${VALAGEN_LOG}"
+    [LOG_MSGE]="None"
 )
 
 declare -A PB_STRUCTURE=(
-	[BW]=50
-	[MP]=100
-	[SLEEP]=0.01
+    [BW]=50
+    [MP]=100
+    [SLEEP]=0.01
 )
 
 TOOL_DBG="false"
@@ -54,186 +54,186 @@ TOOL_LOG="false"
 TOOL_NOTIFY="false"
 
 declare -A VPROJECT=(
-	[NAME]="None"
-	[PATH]="None"
-	[AUTOGEN]="autogen.sh"
-	[CONFIGURE]="configure.ac"
-	[DESKTOP]="desktop.in"
-	[MAKEFILE]="Makefile.am"
-	[README]="README"
-	[COMMENT]="None"
-	[CODE]="None"
+    [NAME]="None"
+    [PATH]="None"
+    [AUTOGEN]="autogen.sh"
+    [CONFIGURE]="configure.ac"
+    [DESKTOP]="desktop.in"
+    [MAKEFILE]="Makefile.am"
+    [README]="README"
+    [COMMENT]="None"
+    [CODE]="None"
 )
 
 #
 # @brief   Main function 
 # @param   None
 # @exitval Function __valagen exit with integer value
-#			0   - tool finished with success operation
-#			128 - missing argument(s) from cli
-#			129 - failed to load tool script configuration from files
-#			130 - failed to load project set configuration from file
-#			131 - missing target directory
+#            0   - tool finished with success operation
+#            128 - missing argument(s) from cli
+#            129 - failed to load tool script configuration from files
+#            130 - failed to load project set configuration from file
+#            131 - missing target directory
 #
 # @usage
-# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 #
 # __valagen
 #
-function __valagen() {
-	local FUNC=${FUNCNAME[0]} MSG="None" STATUS
-	declare -A VPROJECT_STRUCT=(
-		[1]="${VPROJECT[NAME]}" [2]="${VPROJECT[PATH]}"
-		[3]="${VPROJECT[COMMENT]}" [4]="${VPROJECT[CODE]}"
-	)
-	__check_strings VPROJECT_STRUCT
-	STATUS=$?
-	if [ $STATUS -eq $SUCCESS ]; then
-		local FUNC=${FUNCNAME[0]} MSG="None" STATUS_CONF STATUS_CONF_UTIL
-		MSG="Loading basic and util configuration!"
-		__info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
-		__progress_bar PB_STRUCTURE
-		declare -A config_valagen=()
-		__load_conf "$VALAGEN_CFG" config_valagen
-		STATUS_CONF=$?
-		declare -A config_valagen_util=()
-		__load_util_conf "$VALAGEN_UTIL_CFG" config_valagen_util
-		STATUS_CONF_UTIL=$?
-		declare -A STATUS_STRUCTURE=(
-			[1]=$STATUS_CONF [2]=$STATUS_CONF_UTIL
-		)
-		__check_status STATUS_STRUCTURE
-		STATUS=$?
-		if [ $STATUS -eq $NOT_SUCCESS ]; then
-			MSG="Force exit!"
-			__info_debug_message_end "$MSG" "$FUNC" "$VALAGEN_TOOL"
-			exit 129
-		fi
-		TOOL_LOG=${config_valagen[LOGGING]}
-		TOOL_DBG=${config_valagen[DEBUGGING]}
-		TOOL_NOTIFY=${config_valagen[EMAILING]}
-		local PROJECT_SET=${config_valagen_util[PROJECT_SET]}
-		declare -A project_set=()
-		__load_util_conf "${VALAGEN_HOME}/conf/${PROJECT_SET}" project_set
-		STATUS=$?
-		if [ $STATUS -eq $NOT_SUCCESS ]; then
-			MSG="Force exit!"
-			__info_debug_message_end "$MSG" "$FUNC" "$VALAGEN_TOOL"
-			exit 130
-		fi
-		MSG="Generating project structure!"
-		__info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
-		MSG="Checking directory [${VPROJECT[PATH]}/]?"
-		__info_debug_message_que "$MSG" "$FUNC" "$VALAGEN_TOOL"
-		if [ -d "${VPROJECT[PATH]}/" ]; then
-			MSG="[ok]"
-			__info_debug_message_ans "$MSG" "$FUNC" "$VALAGEN_TOOL"
-			local PHOME="${VPROJECT[PATH]}/${VPROJECT[NAME]}"
-			MSG="Generating directory [${PHOME}/]"
-			__info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
-			mkdir "${PHOME}/"
-			local AN=${config_valagen_util[AUTHOR_NAME]} DATE=`date`
-			local AE=${config_valagen_util[AUTHOR_EMAIL]} AL T="	"
-			local ASHT=${project_set[AUTOGEN_SH]} H="#" BSL="\\"
-			local ASHF="${PHOME}/${VPROJECT[AUTOGEN]}" TREE
-			local ASHTF="${VALAGEN_HOME}/conf/${ASHT}"
-			MSG="Generating file [${ASHF}]"
-			__info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
-			while read AL
-			do
-				eval echo "${AL}" >> ${ASHF}
-			done < ${ASHTF}
-			local CT=${project_set[CONFIGURE_AC]}
-			local CF="${PHOME}/${VPROJECT[CONFIGURE]}" CL
-			local CTF="${VALAGEN_HOME}/conf/${CT}"
-			MSG="Generating file [${CF}]"
-			__info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
-			while read CL
-			do
-				eval echo "${CL}" >> ${CF}
-			done < ${CTF}
-			local DT=${project_set[DESKTOP_IN]}
-			local DF="${PHOME}/${VPROJECT[NAME]}.${VPROJECT[DESKTOP]}" DL
-			local DTF="${VALAGEN_HOME}/conf/${DT}"
-			MSG="Generating file [${DF}]"
-			__info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
-			while read DL
-			do
-				eval echo "${DL}" >> ${DF}
-			done < ${DTF}
-			local MSF="${PHOME}/${VPROJECT[MAKEFILE]}"
-			local MST=${project_set[MAKEFILE_AMS]}
-			local MSTF="${VALAGEN_HOME}/conf/${MST}" MSL
-			MSG="Generating file [${MSF}]"
-			__info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
-			while read MSL
-			do
-				eval echo "${MSL}" >> ${MSF}
-			done < ${MSTF}
-			local VT=${project_set[V_SOURCE]} VF="${PHOME}/${VPROJECT[CODE]}"
-			local VTF="${VALAGEN_HOME}/conf/${VT}" VL
-			MSG="Generating file [${VF}]"
-			__info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
-			while read VL
-			do
-				eval echo "${VL}" >> ${VF}
-			done < ${VTF}
-			local VET=${project_set[V_EDIT]}
-			local VETF=$(cat "${VALAGEN_HOME}/conf/${VET}")
-			local VEF="${PHOME}/.editorconfig"
-			MSG="Generating file [${VEF}]"
-			__info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
-			echo -e "${VETF}" > "${VEF}"
-			local RT=${project_set[README]} RF="${PHOME}/${VPROJECT[README]}"
-			local RTF="${VALAGEN_HOME}/conf/${RT}" RL
-			MSG="Generating file [${RF}]"
-			__info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
-			while read RL
-			do
-				eval echo "${RL}" >> ${RF}
-			done < ${RTF}
-			MSG="Set owner!"
-			__info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
-			local USRID=${config_valagen_util[GID]}
-			local GRPID=${config_valagen_util[UID]}
-			eval "chown -R ${USRID}.${GRPID} ${PHOME}/"
-			MSG="Set permission!"
-			__info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
-			eval "chmod -R 700 ${PHOME}/"
-			MSG="Generated Vala project ${PHOME}/"
-			VALAGEN_LOGGING[LOG_MSGE]=$MSG
-			__logging VALAGEN_LOGGING
-			__info_debug_message_end "Done" "$FUNC" "$VALAGEN_TOOL"
-			TREE=$(which tree)
-			__check_tool "${TREE}"
-			STATUS=$?
-			if [ $STATUS -eq $SUCCESS ]; then
-				eval "${TREE} -L 3 ${PHOME}/"
-			fi
-			exit 0
-		fi
-		MSG="[not ok]"
-		__info_debug_message_ans "$MSG" "$FUNC" "$VALAGEN_TOOL"
-		MSG="Generate directory [${VPROJECT[PATH]}/]"
-		__info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
-		MSG="Force exit!"
-		__info_debug_message_end "$MSG" "$FUNC" "$VALAGEN_TOOL"
-		exit 131
-	fi
-	__usage VALAGEN_USAGE
-	exit 128
+function __valagen {
+    local FUNC=${FUNCNAME[0]} MSG="None" STATUS
+    declare -A VPROJECT_STRUCT=(
+        [1]="${VPROJECT[NAME]}" [2]="${VPROJECT[PATH]}"
+        [3]="${VPROJECT[COMMENT]}" [4]="${VPROJECT[CODE]}"
+    )
+    check_strings VPROJECT_STRUCT
+    STATUS=$?
+    if [ $STATUS -eq $SUCCESS ]; then
+        local FUNC=${FUNCNAME[0]} MSG="None" STATUS_CONF STATUS_CONF_UTIL
+        MSG="Loading basic and util configuration!"
+        info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
+        progress_bar PB_STRUCTURE
+        declare -A config_valagen=()
+        load_conf "$VALAGEN_CFG" config_valagen
+        STATUS_CONF=$?
+        declare -A config_valagen_util=()
+        load_util_conf "$VALAGEN_UTIL_CFG" config_valagen_util
+        STATUS_CONF_UTIL=$?
+        declare -A STATUS_STRUCTURE=(
+            [1]=$STATUS_CONF [2]=$STATUS_CONF_UTIL
+        )
+        check_status STATUS_STRUCTURE
+        STATUS=$?
+        if [ $STATUS -eq $NOT_SUCCESS ]; then
+            MSG="Force exit!"
+            info_debug_message_end "$MSG" "$FUNC" "$VALAGEN_TOOL"
+            exit 129
+        fi
+        TOOL_LOG=${config_valagen[LOGGING]}
+        TOOL_DBG=${config_valagen[DEBUGGING]}
+        TOOL_NOTIFY=${config_valagen[EMAILING]}
+        local PROJECT_SET=${config_valagen_util[PROJECT_SET]}
+        declare -A project_set=()
+        load_util_conf "${VALAGEN_HOME}/conf/${PROJECT_SET}" project_set
+        STATUS=$?
+        if [ $STATUS -eq $NOT_SUCCESS ]; then
+            MSG="Force exit!"
+            info_debug_message_end "$MSG" "$FUNC" "$VALAGEN_TOOL"
+            exit 130
+        fi
+        MSG="Generating project structure!"
+        info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
+        MSG="Checking directory [${VPROJECT[PATH]}/]?"
+        info_debug_message_que "$MSG" "$FUNC" "$VALAGEN_TOOL"
+        if [ -d "${VPROJECT[PATH]}/" ]; then
+            MSG="[ok]"
+            info_debug_message_ans "$MSG" "$FUNC" "$VALAGEN_TOOL"
+            local PHOME="${VPROJECT[PATH]}/${VPROJECT[NAME]}"
+            MSG="Generating directory [${PHOME}/]"
+            info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
+            mkdir "${PHOME}/"
+            local AN=${config_valagen_util[AUTHOR_NAME]} DATE=`date`
+            local AE=${config_valagen_util[AUTHOR_EMAIL]} AL T="    "
+            local ASHT=${project_set[AUTOGEN_SH]} H="#" BSL="\\"
+            local ASHF="${PHOME}/${VPROJECT[AUTOGEN]}" TREE
+            local ASHTF="${VALAGEN_HOME}/conf/${ASHT}"
+            MSG="Generating file [${ASHF}]"
+            info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
+            while read AL
+            do
+                eval echo "${AL}" >> ${ASHF}
+            done < ${ASHTF}
+            local CT=${project_set[CONFIGURE_AC]}
+            local CF="${PHOME}/${VPROJECT[CONFIGURE]}" CL
+            local CTF="${VALAGEN_HOME}/conf/${CT}"
+            MSG="Generating file [${CF}]"
+            info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
+            while read CL
+            do
+                eval echo "${CL}" >> ${CF}
+            done < ${CTF}
+            local DT=${project_set[DESKTOP_IN]}
+            local DF="${PHOME}/${VPROJECT[NAME]}.${VPROJECT[DESKTOP]}" DL
+            local DTF="${VALAGEN_HOME}/conf/${DT}"
+            MSG="Generating file [${DF}]"
+            info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
+            while read DL
+            do
+                eval echo "${DL}" >> ${DF}
+            done < ${DTF}
+            local MSF="${PHOME}/${VPROJECT[MAKEFILE]}"
+            local MST=${project_set[MAKEFILE_AMS]}
+            local MSTF="${VALAGEN_HOME}/conf/${MST}" MSL
+            MSG="Generating file [${MSF}]"
+            info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
+            while read MSL
+            do
+                eval echo "${MSL}" >> ${MSF}
+            done < ${MSTF}
+            local VT=${project_set[V_SOURCE]} VF="${PHOME}/${VPROJECT[CODE]}"
+            local VTF="${VALAGEN_HOME}/conf/${VT}" VL
+            MSG="Generating file [${VF}]"
+            info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
+            while read VL
+            do
+                eval echo "${VL}" >> ${VF}
+            done < ${VTF}
+            local VET=${project_set[V_EDIT]}
+            local VETF=$(cat "${VALAGEN_HOME}/conf/${VET}")
+            local VEF="${PHOME}/.editorconfig"
+            MSG="Generating file [${VEF}]"
+            info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
+            echo -e "${VETF}" > "${VEF}"
+            local RT=${project_set[README]} RF="${PHOME}/${VPROJECT[README]}"
+            local RTF="${VALAGEN_HOME}/conf/${RT}" RL
+            MSG="Generating file [${RF}]"
+            info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
+            while read RL
+            do
+                eval echo "${RL}" >> ${RF}
+            done < ${RTF}
+            MSG="Set owner!"
+            info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
+            local USRID=${config_valagen_util[GID]}
+            local GRPID=${config_valagen_util[UID]}
+            eval "chown -R ${USRID}.${GRPID} ${PHOME}/"
+            MSG="Set permission!"
+            info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
+            eval "chmod -R 700 ${PHOME}/"
+            MSG="Generated Vala project ${PHOME}/"
+            VALAGEN_LOGGING[LOG_MSGE]=$MSG
+            logging VALAGEN_LOGGING
+            info_debug_message_end "Done" "$FUNC" "$VALAGEN_TOOL"
+            TREE=$(which tree)
+            check_tool "${TREE}"
+            STATUS=$?
+            if [ $STATUS -eq $SUCCESS ]; then
+                eval "${TREE} -L 3 ${PHOME}/"
+            fi
+            exit 0
+        fi
+        MSG="[not ok]"
+        info_debug_message_ans "$MSG" "$FUNC" "$VALAGEN_TOOL"
+        MSG="Generate directory [${VPROJECT[PATH]}/]"
+        info_debug_message "$MSG" "$FUNC" "$VALAGEN_TOOL"
+        MSG="Force exit!"
+        info_debug_message_end "$MSG" "$FUNC" "$VALAGEN_TOOL"
+        exit 131
+    fi
+    usage VALAGEN_USAGE
+    exit 128
 }
 
 #
 # @brief   Main entry point
 # @params  Values required project name, project path and comment
 # @exitval Script tool valagen exit with integer value
-#			0   - tool finished with success operation
-#			127 - run tool script as root user from cli
-#			128 - missing argument(s) from cli
-#			129 - failed to load tool script configuration from files
-#			130 - failed to load project set configuration from file
-#			131 - missing target directory
+#            0   - tool finished with success operation
+#            127 - run tool script as root user from cli
+#            128 - missing argument(s) from cli
+#            129 - failed to load tool script configuration from files
+#            130 - failed to load project set configuration from file
+#            131 - missing target directory
 #
 VPROJECT[NAME]="$1"
 VPROJECT[PATH]="$2"
@@ -241,10 +241,10 @@ VPROJECT[COMMENT]="$3"
 VPROJECT[CODE]="${1}.vala"
 
 printf "\n%s\n%s\n\n" "${VALAGEN_TOOL} ${VALAGEN_VERSION}" "`date`"
-__check_root
+check_root
 STATUS=$?
 if [ $STATUS -eq $SUCCESS ]; then
-	__valagen
+    __valagen
 fi
 
 exit 127
